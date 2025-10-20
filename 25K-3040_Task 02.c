0777c8cbceb3af55b7ff7f8aba6553e7db7e624a
Moiz Mustafa <k253040@nu.edu.pk>
2025-10-20 15:37:53 +0500

    Add files via upload

---
+++ 25K-3040_Task 02.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n=0;
+	printf("Enter the number of elements in the array:\n");
+	scanf("%d", &n);
+	int arr[n];
+	printf("Enter the values for the array:\n");
+	int i=0;
+	for(i=0; i<n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	for(i=n-1; i>=0; i--)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
